c1b66878160b6c9abe34ddbbe95dd2875af8f449
acavalls <alejandro.canosa@bsc.es>
2022-07-04 17:01:27 +0200

    MongoDB: Modifying configuration for working on different environments

--- src/outbox_db.js
+++ src/outbox_db.js
@@ -1,17 +1,17 @@
 import mongoose from 'mongoose';
 import winston from 'winston';
 
-require('dotenv').config();
-
 export default callback => {
-	let host = process.env.NODE_ENV === 'test' ? process.env.MONGO_HOST_TEST : process.env.MONGO_HOST;	
-	let db = process.env.NODE_ENV === 'test' ? process.env.MONGO_DB_OUTBOX_TEST : process.env.MONGO_DB_OUTBOX;
+	let host = process.env.NODE_ENV === 'prod' ? process.env.MONGO_HOST : process.env.MONGO_HOST_TEST;	
+	let db = process.env.NODE_ENV === 'prod' ? process.env.MONGO_DB_OUTBOX : process.env.MONGO_DB_OUTBOX_TEST;
 	let username = process.env.MONGO_USER;
 	let password = process.env.MONGO_PASS;
 	let authSource = process.env.MONGO_AUTH;
         let replicaSet = process.env.MONGO_REPLICASET;
-	// connect to MongoDB, then pass it to the callback fn:
-	const mongo = mongoose.connect(`mongodb://${username}:${password}@${host}/${db}??connectTimeoutMS=300000&replicaSet=${replicaSet}&authSource=${authSource}`)
-		.then(() => winston.info(`Connected to ${db}`))
-	callback(mongo); 
+        // connect to MongoDB, then pass it to the callback fn:
+        let uri = process.env.NODE_ENV === 'prod' ? `mongodb://${username}:${password}@${host}/${db}?authSource=${authSource}` :
+            `mongodb://${username}:${password}@${host}/${db}?connectTimeoutMS=300000&replicaSet=rs0&authSource=${authSource}`
+        const mongo = mongoose.connect(uri)
+            .then(() => winston.info(`Connected to ${db}`))
+        callback(mongo);
 }
--- src/permissions_db.js
+++ src/permissions_db.js
@@ -1,13 +1,15 @@
 let MongoClient = require('mongodb').MongoClient
 let db;
-let host = process.env.NODE_ENV === 'test' ? process.env.MONGO_HOST_TEST : process.env.MONGO_HOST;
-let permissions_db = process.env.NODE_ENV === 'test' ? process.env.MONGO_DB_PERMISSIONS_TEST : process.env.MONGO_DB_PERMISSIONS;
+let host = process.env.NODE_ENV === 'prod' ? process.env.MONGO_HOST : process.env.MONGO_HOST_TEST;
+let permissions_db = process.env.NODE_ENV === 'prod' ? process.env.MONGO_DB_PERMISSIONS : process.env.MONGO_DB_PERMISSIONS_TEST	;
 let username = process.env.MONGO_USER;
 let password = process.env.MONGO_PASS;
 let authSource = process.env.MONGO_AUTH;
 let replicaSet = process.env.MONGO_REPLICASET;
+let uri = process.env.NODE_ENV === 'prod' ? `mongodb://${username}:${password}@${host}/${db}?authSource=${authSource}` :
+            `mongodb://${username}:${password}@${host}/${db}?connectTimeoutMS=300000&replicaSet=rs0&authSource=${authSource}`
 
-MongoClient.connect(`mongodb://${username}:${password}@${host}/${permissions_db}?connectTimeoutMS=300000&replicaSet=${replicaSet}&authSource=${authSource}`, function (err, client) {
+MongoClient.connect(uri, function (err, client) {
   	if (err) throw err
 	console.log("Permissions api: successfully connected")
 	console.log("Permissions HOST: ", host)
